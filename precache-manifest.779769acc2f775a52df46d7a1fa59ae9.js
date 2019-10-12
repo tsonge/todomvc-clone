56be281e8691fb29a13ac205ac81e9eb6002750f
tsonge <tsonge44@gmail.com>
2019-10-12 13:40:57 -0400

    Updates

---
+++ precache-manifest.779769acc2f775a52df46d7a1fa59ae9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3b3bf08299002d1fc3c92efce869f9d",
+    "url": "/todomvc-clone/index.html"
+  },
+  {
+    "revision": "973d3ba7c9ca75e66529",
+    "url": "/todomvc-clone/static/css/main.564b568b.chunk.css"
+  },
+  {
+    "revision": "4683b01fa37d6f33ebfe",
+    "url": "/todomvc-clone/static/js/2.91ed0f0a.chunk.js"
+  },
+  {
+    "revision": "973d3ba7c9ca75e66529",
+    "url": "/todomvc-clone/static/js/main.2c18f367.chunk.js"
+  },
+  {
+    "revision": "669d6018a48f95a0063c",
+    "url": "/todomvc-clone/static/js/runtime-main.38804867.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9d43c0522f197aa5a4d7ba9781955a7f.js"
+  "/todomvc-clone/precache-manifest.779769acc2f775a52df46d7a1fa59ae9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/todomvc-clone/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
