00866ee0ca193e112afed3c4904e298f252535ec
tsonge <tsonge44@gmail.com>
2019-10-12 11:37:32 -0400

    Updates

---
+++ precache-manifest.813eb2edd8b2a53f8883eb32422b3186.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68c4b239f6e44d575cd1b6d195fb1ba0",
+    "url": "/tsonge/todomvc-clone/index.html"
+  },
+  {
+    "revision": "973d3ba7c9ca75e66529",
+    "url": "/tsonge/todomvc-clone/static/css/main.564b568b.chunk.css"
+  },
+  {
+    "revision": "4683b01fa37d6f33ebfe",
+    "url": "/tsonge/todomvc-clone/static/js/2.91ed0f0a.chunk.js"
+  },
+  {
+    "revision": "973d3ba7c9ca75e66529",
+    "url": "/tsonge/todomvc-clone/static/js/main.2c18f367.chunk.js"
+  },
+  {
+    "revision": "8f3320079a7f32a49803",
+    "url": "/tsonge/todomvc-clone/static/js/runtime-main.77519e0c.js"
+  }
+]);
